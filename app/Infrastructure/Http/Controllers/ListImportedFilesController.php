613c730ea105bdb2fd82af618ac05a5363b0118a
Leonam Pereira <leogonp@gmail.com>
2024-05-16 06:24:01 -0300

    feat: listing endpoint

---
+++ app/Infrastructure/Http/Controllers/ListImportedFilesController.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Infrastructure\Http\Controllers;
+
+use App\Application\Services\ImportCSVService;
+use App\Infrastructure\Http\Controllers\Request\ImportCSVRequest;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Routing\Controller;
+use Symfony\Component\HttpFoundation\Response;
+use Throwable;
+
+class ImportCsvController extends Controller
+{
+    public function __construct(
+        private readonly ImportCSVService $service
+    ) {
+    }
+
+    public function __invoke(ImportCSVRequest $request): JsonResponse
+    {
+        try {
+            ($this->service)($request->toFile());
+
+            return response()->json(
+                ['message' => 'Import was successfully made.'],
+                Response::HTTP_CREATED,
+            );
+        } catch (Throwable $e) {
+            return response()->json(
+                ['message' => $e->getMessage()],
+                $e->getCode() ?: Response::HTTP_INTERNAL_SERVER_ERROR,
+            );
+        }
+    }
+}
---
+++ tests/Unit/Infrastructure/Http/Controllers/ListImportedFilesControllerTest.php
@@ -0,0 +1,79 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Unit\Infrastructure\Http\Controllers;
+
+use App\Application\Services\ImportCSVService;
+use App\Infrastructure\Http\Controllers\ImportCsvController;
+use App\Infrastructure\Http\Controllers\Request\ImportCSVRequest;
+use Exception;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\UploadedFile;
+use Symfony\Component\HttpFoundation\Response;
+use Tests\TestCase;
+
+class ImportCsvControllerTest extends TestCase
+{
+    private ImportCSVService $service;
+    private ImportCsvController $controller;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        $this->service = $this->mock(ImportCSVService::class);
+
+        $this->controller = app(ImportCsvController::class);
+
+    }
+
+    public function testSuccessfulImport()
+    {
+        $this->service
+            ->shouldReceive('__invoke')
+            ->once()
+            ->with($this->isInstanceOf(UploadedFile::class));
+
+        $uploadedFile = UploadedFile::fake()->create('data.csv');
+
+        $request = $this->mock(ImportCSVRequest::class);
+
+        $request->shouldReceive('toFile')
+            ->once()
+            ->andReturn($uploadedFile);
+
+        $response = ($this->controller)($request);
+
+        $this->assertInstanceOf(JsonResponse::class, $response);
+        $this->assertEquals(Response::HTTP_CREATED, $response->getStatusCode());
+        $this->assertJson($response->getContent());
+        $this->assertArrayHasKey('message', json_decode($response->getContent(), true));
+        $this->assertEquals('Import was successfully made.', json_decode($response->getContent(), true)['message']);
+    }
+
+    public function testWillThrownExceptionResponseOnFailure()
+    {
+        $this->service
+            ->shouldReceive('__invoke')
+            ->once()
+            ->with($this->isInstanceOf(UploadedFile::class))
+            ->andThrow(new Exception('Failed to import CSV'));
+
+        $uploadedFile = UploadedFile::fake()->create('data.csv');
+
+        $request = $this->mock(ImportCSVRequest::class);
+
+        $request->shouldReceive('toFile')
+            ->once()
+            ->andReturn($uploadedFile);
+
+        $response = ($this->controller)($request);
+
+        $this->assertInstanceOf(JsonResponse::class, $response);
+        $this->assertEquals(Response::HTTP_INTERNAL_SERVER_ERROR, $response->getStatusCode());
+        $this->assertJson($response->getContent());
+        $this->assertArrayHasKey('message', json_decode($response->getContent(), true));
+        $this->assertEquals('Failed to import CSV', json_decode($response->getContent(), true)['message']);
+    }
+}
